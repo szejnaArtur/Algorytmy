b7e3dcb3e9020d912171d28a59c64d2b6aaa57bb
Artur <szejnaartur@gmail.com>
2020-04-07 20:44:19 +0200

    Fibonnacci sequence

---
+++ src/main/java/pl/arturszejna/algorithms/fibonnacciSequence/App.java
@@ -0,0 +1,8 @@
+package pl.arturszejna.algorithms.fibonnacciSequence;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println(Fibonnacci.displayRecursively(5));
+        System.out.println(Fibonnacci.displayNonRecursively(50));
+    }
+}
---
+++ src/main/java/pl/arturszejna/algorithms/fibonnacciSequence/Fibonnacci.java
@@ -0,0 +1,33 @@
+package pl.arturszejna.algorithms.fibonnacciSequence;
+
+//Write a function to find the nth number in a fibonnacci sequence.
+
+class Fibonnacci {
+    static int displayRecursively(int n) {
+        if (n < 1) {
+            throw new IllegalArgumentException("numbers below 1 not allowed");
+        }
+        if (n == 1 || n == 2) {
+            return 1;
+        } else {
+            return displayRecursively(n - 1) + displayRecursively(n - 2);
+        }
+    }
+
+    static int displayNonRecursively(int n) {
+        if (n < 1) throw new IllegalArgumentException("numbers below 1 not allowed");
+        if (n<=2) return 1;
+
+        int first = 1;
+        int second = 1;
+        int nth = 2;
+
+        for(int i = 3; i<=n; i++){
+            nth = first + second;
+            first = second;
+            second = nth;
+        }
+
+        return nth;
+    }
+}
