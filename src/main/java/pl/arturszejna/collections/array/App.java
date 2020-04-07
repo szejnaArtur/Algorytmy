393c2ce356b7d1768cdb9a0c162be5e076be42f5
Artur <szejnaartur@gmail.com>
2020-04-07 22:00:56 +0200

    Write a function that deleted all duplicate values in an array?

--- src/main/java/pl/arturszejna/collections/array/App.java
+++ src/main/java/pl/arturszejna/collections/array/App.java
@@ -2,7 +2,14 @@ package pl.arturszejna.collections.array;
 
 public class App {
     public static void main(String[] args) {
-        int[] array = {1, 2, 3, 4, 5};
-        Array.checkForDuplicates(array);
+        int[] array = {1, 2, 3, 4, 5, 5, 1, 2, 8, 11, 11, 8};
+        Array.isDuplicateMessage(array);
+
+        array = Array.removeAllDuplicates(array);
+
+        for ( Integer x : array ) {
+            System.out.print(x + " ");
+        }
+
     }
 }
--- src/main/java/pl/arturszejna/collections/array/Array.java
+++ src/main/java/pl/arturszejna/collections/array/Array.java
@@ -4,21 +4,45 @@ import java.util.HashSet;
 import java.util.Set;
 
 class Array {
-    static private String isDuplicates(int[] myArray) {
+    static private String isDuplicatePrivateMessage(int[] myArray) {
         Set<Integer> mySet = new HashSet<Integer>();
 
-
-        for ( Integer x : myArray ) {
-            if (!mySet.add(x)) {
+        for ( Integer x : myArray )
+            if (!mySet.add(x))
                 return "a duplicate exist";
-            }
-        }
         return "a duplicate not exist";
     }
 
-    static void checkForDuplicates(int[] myArray) {
+    static private boolean isDuplicate(int[] myArray) {
         Set<Integer> mySet = new HashSet<Integer>();
-        System.out.println(isDuplicates(myArray));
+
+        for ( Integer x : myArray )
+            if (!mySet.add(x))
+                return true;
+
+        return false;
+    }
+
+    static void isDuplicateMessage(int[] myArray) {
+        System.out.println(isDuplicatePrivateMessage(myArray));
+    }
+
+    static int[] removeAllDuplicates(int[] myArray){
+        if(isDuplicate(myArray)) {
+            Set<Integer> set = new HashSet<Integer>();
+
+            for ( Integer x : myArray )
+                set.add(x);
+
+            int[] newArray = new int[set.size()];
+            int i = 0;
+
+            for ( Integer x : set )
+                newArray[i++] = x;
+
+            return newArray;
+        }
+        return myArray;
     }
 
 }
